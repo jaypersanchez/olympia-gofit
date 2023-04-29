6abb70a26ac1474c5ea919b06eeec7f7b0eb759a
Adrian Estopace <estopaceadrian@gmail.com>
2023-04-29 18:50:31 +0800

    created reusable components and screens 9-14

---
+++ src/screens/accountSetup/GetStarted.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import LeftCheckBox from './components/LeftCheckBox';
+
+const GetStarted = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxPress = (value) => {
+    setIsChecked(value);
+  };
+
+  const handleButtonPress = () => {
+    console.log('Generate workout plan button pressed');
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Great!</Text>
+        <Text style={styles.title}>Let's Workout!</Text>
+      </View>
+      <View style={styles.checkboxContainer}>
+        <LeftCheckBox
+          label="I agree to the terms and conditions"
+          isChecked={isChecked}
+          onPress={handleCheckboxPress}
+        />
+      </View>
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        disabled={!isChecked}
+        onPress={handleButtonPress}
+      >
+        <Text
+          style={[styles.buttonText, !isChecked && styles.buttonTextDisabled]}
+        >
+          Generate workout plan
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  titleContainer: {
+    marginBottom: 32,
+  },
+  title: {
+    fontSize: 46,
+    fontWeight: 'bold',
+    color: '#6842FF',
+    textAlign: 'center',
+  },
+  checkboxContainer: {
+    marginBottom: 24,
+  },
+  buttonContainer: {
+    backgroundColor: '#6842FF',
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    borderRadius: 100,
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 22.4,
+    color: '#fff',
+    textAlign: 'center',
+  },
+  buttonTextDisabled: {
+    opacity: 0.5,
+  },
+});
+export default GetStarted;
