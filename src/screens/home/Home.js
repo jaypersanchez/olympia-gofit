7601bc1ff2095ac4a668d497d73a53afcb885a8c
romar <romar.doinog@gmail.com>
2023-06-09 16:12:15 +0800

    update homescreen name and login

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -8,16 +8,38 @@ import Banner from "./components/Banner";
 import ExcerciseCard from "./components/ExcerciseCard";
 import Streak from "./components/Streak";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { BackHandler } from "react-native";
 
 const Home = ({ navigation, route }) => {
-  const frequency = useSelector((state) => state.onboarding.frequency);
+  const user_data = useSelector((state) => state.onboarding);
   const { data } = dailyexercise || [];
 
-  console.log("dailyexcercise", frequency);
+  const handleName = (name) => {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  };
+
+  useEffect(() => {
+    const backAction = () => {
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
+  console.log("dailyexcercise", user_data);
   return (
     <View style={styles.container}>
       <View style={{ gap: 24, width: "100%" }}>
-        <TextItem type="h3">Welcome, Christina!</TextItem>
+        <TextItem type="h3">
+          {"Welcome, "}
+          {handleName(user_data.name)}!
+        </TextItem>
         <ScrollView
           contentContainerStyle={{
             flexGrow: 1,
@@ -31,7 +53,7 @@ const Home = ({ navigation, route }) => {
           <Banner week={1} day={12} excercise={"Dead Lift 3 sets of 8 reps"} />
           <TextItem type="h5">Daily Excercise</TextItem>
           <View style={{ gap: 16 }}>
-            {data.slice(0, frequency).map((item) => (
+            {data.slice(0, user_data.frequency).map((item) => (
               <ExcerciseCard
                 key={item.id}
                 day={item.day}
